15bcd6ee706b53542765463c4f3d2e26ca156c96
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2022-09-20 19:02:54 -0400

    not done

--- checkerboard3x3.cpp
+++ checkerboard3x3.cpp
@@ -11,13 +11,23 @@ int check(int width, int height){
     std::cout << "Input width: " << width << "\nInput height: " << height << std::endl;
 
     std::cout << "Shape:" << std::endl;
-
-    for (int board = 0; board < width; board++){
-        for (int col = 0; col < 3; col++){
-            std::cout << "*";
+    
+    for (int row = 0; row < 3; row++){
+        for (int board = 0; board < width; board++){
+            for (int col = 0; col < 3; col++){
+                std::cout << "*";
+                
+            }
+            for (int i = 0; i < 3; i++){
+                std::cout << " ";
+            }
         }
+        std::cout << std::endl;
     }
 
     std::cout << std::endl;
     return(width,height);
+}
+int main(){
+    check(16,11);
 }
